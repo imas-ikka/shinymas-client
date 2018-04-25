4b92fd2770f1dab59211620b1d2b039dd378a079
kumapana <kumachan05-11@hotmail.co.jp>
2018-04-25 14:16:56 +0900

    リリース準備 リファクタリングなどを

--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 Electronで書かれたアイドルマスター シャイニーカラーズの非公式
 
 Electronの練習がてら書いたものなので色々ヤバイところがあるかと思いますので、issueだったりプルリク送っていただけると幸いです。
+
+# 開発環境および動作検証環境
+
+## 開発環境
+OS : ArchLinux 4.15.15-1-ARCH
+Electron : v1.8.4
+
+## 検証環境
+OS : ArchLinux 4.15.15-1-ARCH
+Electron : v1.8.4
+
+OS : LinuxMint 18.3 Sylvia (Linux 4.13.16-041316-generic)
+Electron : v1.8.4
+
+OS : Windows 8.1 Pro with Update 64bit 
+Electron : v1.8.4
+
+macOSではまだ動作の検証を行っていません。確認が取れ次第報告いたします。
+
+# 免責事項
+当コードおよびリリースした実行形式ファイルを使用したことで発生したいかなる損害の一切の責任を負いません。
+自己責任でお使い下さい。
--- src/main.js
+++ src/main.js
@@ -1,60 +1,26 @@
-// アプリケーション作成用のモジュールを読み込み
-const electron = require('electron');
+"use strict";
+
+const electron = require("electron");
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
- 
-const path = require('path');
-const url = require('url');
- 
-// メインウィンドウ
 let mainWindow;
- 
 
-function createWindow() {
-  // メインウィンドウを作成します
-  mainWindow = new BrowserWindow({
-    "width": 1136,
-    "height": 664
-  });
- 
-  // メインウィンドウに表示するURLを指定します
-  // （今回はmain.jsと同じディレクトリのindex.html）
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
- 
-  // mainWindow.loadURL(url.format({
-  //   pathname: path.join('shinycolors.enza.fun'),
-  //   protocol: 'https:',
-  //   slashes: true
-  // }));
 
-  // デベロッパーツールの起動
-  //mainWindow.webContents.openDevTools();
- 
-  // メインウィンドウが閉じられたときの処理
-  mainWindow.on('closed', () => {
+// Electronの初期化完了後に実行
+app.on('ready', function() {
+  // メイン画面の表示。ウィンドウの幅、高さを指定
+  mainWindow = new BrowserWindow({width: 1136, height: 640});
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // ウィンドウが閉じられたらアプリも終了
+  mainWindow.on('closed', function() {
     mainWindow = null;
   });
-}
- 
-//  初期化が完了した時の処理
-app.on('ready', createWindow);
- 
-// 全てのウィンドウが閉じたときの処理
-app.on('window-all-closed', () => {
-  // macOSのとき以外はアプリケーションを終了させます
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
 });
-// アプリケーションがアクティブになった時の処理(Macだと、Dockがクリックされた時）
-app.on('activate', () => {
-  // メインウィンドウが消えている場合は再度メインウィンドウを作成する
-  if (mainWindow === null) {
-    Electron.session.defaultSession.clearCache(() => {})
-    createWindow();
+
+// macOSの場合全てのウィンドウが閉じたら終了
+app.on('window-all-closed', function() {
+  if (process.platform != 'darwin') {
+    app.quit();
   }
 });
